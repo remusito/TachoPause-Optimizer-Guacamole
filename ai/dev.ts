2b4367a37ed2348d8361429873616eb11a5f34e2
remusito <rcerezomartin@gmail.com>
2025-10-23 17:12:21 +0200

    Delete ai directory

--- ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/optimize-route-breaks.ts';
-import '@/ai/flows/calculate-pause-time.ts';
-import '@/ai/flows/calculate-route-details.ts';
