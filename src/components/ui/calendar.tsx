e39c14f839e7338fc881c4b90e5649ef564387a1
rcerezomartin <rcerezomartin@gmail.com>
2025-10-24 21:04:16 +0000

    Fix: Corrige los nombres de las propiedades de los iconos en el calendario

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,10 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        icon_left: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }) => (
+        icon_right: ({ className, ...props }) => (
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
