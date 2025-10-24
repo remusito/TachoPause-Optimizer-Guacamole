128178f0da622633b6e0616a346b487dbe1e83d8
rcerezomartin <rcerezomartin@gmail.com>
2025-10-24 22:09:56 +0000

    refactor: Use environment variables for Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,13 +3,13 @@ import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVaDyb6BSQTGl1-WYn1nTRzwNk2YvNpPo",
-  authDomain: "tachopause-optimizer.firebaseapp.com",
-  projectId: "tachopause-optimizer",
-  storageBucket: "tachopause-optimizer.firebasestorage.app",
-  messagingSenderId: "603408014533",
-  appId: "1:603408014533:web:16e27711d27dcbda0d64ac",
-  measurementId: "G-SD46WRPTQP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
